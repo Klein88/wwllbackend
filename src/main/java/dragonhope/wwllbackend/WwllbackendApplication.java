f4e23d5f9cbcee64bf93363bc30089ac11ed8fd7
Klein88 <1440984389@qq.com>
2024-12-29 14:33:53 +0800

    Initial commit

---
+++ src/main/java/dragonhope/wwllbackend/WwllbackendApplication.java
@@ -0,0 +1,13 @@
+package dragonhope.wwllbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WwllbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WwllbackendApplication.class, args);
+	}
+
+}
